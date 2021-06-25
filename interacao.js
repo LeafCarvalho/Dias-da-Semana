d76100fe375136daaf29d380e5a31d761d9ea109
Rafael Carvalho <rafaeldecarvalhodasilva01@yahoo.com.br>
2021-06-25 09:09:02 -0300

    Dias da semana feito

---
+++ interacao.js
@@ -0,0 +1,23 @@
+function semana() {
+    sem = document.getElementById('weekday', 'keypress', function)
+    if (sem == value || sem == '') {
+        alert('É necessário que se digite um dia da semana!')
+    } else {}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+/* 
+- FOR de LABEL terá o mesmo nome que a ID e NAME do input
+- Input tendo LIST terá o mesmo nome da ID de DATALIST.
+- O VALUE de OPTIONS já dá opções, não é necessário fechar a tag.
+*/
