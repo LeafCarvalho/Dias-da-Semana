a584308fc60ab8132f9c8d09e4c78e3e50e40e86
Rafael Carvalho <79648062+LeafCarvalho@users.noreply.github.com>
2021-10-28 19:14:56 -0300

    refazendo

--- interacao.js
+++ interacao.js
@@ -1,36 +1,7 @@
-let dias = document.getElementById('weekday')
-let tar = document.getElementById('tarefas')
-let resp = document.getElementById('res')
-
-function semanatarefa(Dias, Tarefas) {
-
-var qd = document.getElementById('quadro')
-var qtdLinhas = qd.rows.length
-var linha = qd.insertRow(qtdLinhas)
-
-var cellDiasdaSemana = linha.insertCell(0)
-var cellTarefas = linha.insertCell(1)
-
-cellDiasdaSemana = qtdLinhas
-cellTarefas = qtdLinhas
-
-
-
-}
-
-
-function OK() {
-    document.getElementById('tarefas').focus(); // Serve para mudar de input automaticamente. Ps:Obrigado Arthur! //
-    if (dias.value.length == 0 || dias == Number) {
-        alert('Gentileza digitar um valor válido!')
-    } else {}
-}
 
 
 function prosseguir() {
-    if (tar.value.length == 0) {
-    alert('Digite uma tarefa!')
-    } else {}
+
 }
 
 
