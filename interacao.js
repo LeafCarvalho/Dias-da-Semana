58e7065d9fd1f4b9bce0025a1ec0832da1cf3ca0
Rafael Carvalho <79648062+LeafCarvalho@users.noreply.github.com>
2021-11-07 23:25:26 -0300

    organizando2

--- interacao.js
+++ interacao.js
@@ -1,1 +1,15 @@
-const Prosseguir = document.getElementById('Prosseguir')
+let Prosseguir = document.querySelector('ok-container')
+let feira = document.getElementById('weekday')
+let afazeres = document.querySelector('container')
+let table = document.createElement('table')
+
+Prosseguir = () => {
+    let dias = []
+    let tarefas = []
+
+    dias.push
+    tarefas.push
+    table.innerHTML = ''
+
+    
+}
